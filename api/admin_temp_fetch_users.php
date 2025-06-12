a4e09d2a1bc22fa4a8b07efbcdb4fb7740fe6bae
qwaxxx <raymart.poras@gmail.com>
2025-06-13 01:36:43 +0800

    06/13/2025-12:10

--- api/admin_temp_fetch_users.php
+++ api/admin_temp_fetch_users.php
@@ -2,22 +2,12 @@
 include 'conn.php';
 $result = $conn->query("SELECT id, name, email, contact, user_type, create_on FROM users");
 
+$users = array();
 while ($row = $result->fetch_assoc()) {
-    echo "<tr>
-        <td>{$row['id']}</td>
-        <td>{$row['name']}</td>
-        <td>{$row['email']}</td>
-        <td>{$row['contact']}</td>
-        <td>{$row['user_type']}</td>
-        <td>{$row['create_on']}</td>
-        <td>
-            <button class='btn btn-sm btn-warning' onclick='editUser({$row['id']})'>Edit</button>";
-    
     // Only show Delete button if user_type is NOT admin
-    if ($row['user_type'] !== 'admin') {
-        echo " <button class='btn btn-sm btn-danger' onclick='deleteUser({$row['id']})'>Delete</button>";
-    }
-
-    echo "</td>
-    </tr>";
+    $row['can_delete'] = $row['user_type'] !== 'admin';
+    $users[] = $row;
 }
+
+header('Content-Type: application/json');
+echo json_encode($users);
